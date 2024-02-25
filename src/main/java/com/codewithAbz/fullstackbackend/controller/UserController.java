f22859c284b2837441b75d6731992cbe21ec3723
Abiye <mieabiye.selema@kainos.com>
2024-02-25 00:35:26 +0000

    Allow you to edit User role

--- src/main/java/com/codewithAbz/fullstackbackend/controller/UserController.java
+++ src/main/java/com/codewithAbz/fullstackbackend/controller/UserController.java
@@ -103,6 +103,7 @@ public class UserController {
                     user.setUsername(newUser.getUsername());
                     user.setName(newUser.getName());
                     user.setEmail(newUser.getEmail());
+                    user.setRole(newUser.getRole());
                     return userRepository.save(user);
                 }).orElseThrow(() -> new UserNotFoundException(id));
     }
